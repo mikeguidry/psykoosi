ef59221141238a8878b0bd8b70532798b3886c0e
Filipp <ozinov@quantion.ru>
2014-08-17 17:01:49 +0400

    Added swig library

---
+++ depends/swig/Include/swigconfig.h
@@ -0,0 +1,102 @@
+/* Source/Include/swigconfig.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if the system has the type `bool'. */
+#define HAVE_BOOL 1
+
+/* define if the Boost library is available */
+#undef HAVE_BOOST
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#undef HAVE_INTTYPES_H
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#undef HAVE_LIBDL
+
+/* Define to 1 if you have the `dld' library (-ldld). */
+#undef HAVE_LIBDLD
+
+/* Define to 1 if you have the <memory.h> header file. */
+#undef HAVE_MEMORY_H
+
+/* Define if you have PCRE library */
+#undef HAVE_PCRE
+
+/* Define if popen is available */
+#undef HAVE_POPEN
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#undef HAVE_STDLIB_H
+
+/* Define to 1 if you have the <strings.h> header file. */
+#undef HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#undef HAVE_STRING_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#undef HAVE_SYS_STAT_H
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#undef HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#undef HAVE_UNISTD_H
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+#undef NO_MINUS_C_MINUS_O
+
+/* Name of package */
+#undef PACKAGE
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "unknown"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "swig"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "swig 1"
+
+/* Define to the one symbol short name of this package. */
+#undef PACKAGE_TARNAME
+
+/* Define to the home page for this package. */
+#undef PACKAGE_URL
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1"
+
+/* The size of `void *', as computed by sizeof. */
+#undef SIZEOF_VOID_P
+
+/* Define to 1 if you have the ANSI C header files. */
+#undef STDC_HEADERS
+
+/* Compiler that built SWIG */
+#define SWIG_CXX "unknown"
+
+/* Directory for SWIG system-independent libraries */
+//#define SWIG_LIB "/home/fippo/koding/psykoosi/depends/swig/Lib/"
+
+/* Directory for SWIG system-independent libraries (Unix install on native
+   Windows) */
+#undef SWIG_LIB_WIN_UNIX
+
+/* Platform that SWIG is built for */
+#define SWIG_PLATFORM "multi"
+
+/* Version number of package */
+#undef VERSION
+
+
+/* Default language */
+#define SWIG_LANG               "-tcl"
+
+/* Deal with attempt by Microsoft to deprecate C standard runtime functions */
+#if defined(_MSC_VER)
+# define _CRT_SECURE_NO_DEPRECATE
+#endif
+
