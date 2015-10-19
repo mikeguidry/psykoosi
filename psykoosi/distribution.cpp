d619af29c075b34e5e69f6f330b6ab4d407d533a
Mike Guidry <mike@unifieddefense.net>
2015-10-19 04:13:10 -0400

    forgot a couple files

---
+++ psykoosi/distribution.cpp
@@ -0,0 +1,20 @@
+/* distribution of fuzzing tasks
+
+   this needs to allow direct access to all classes to insert the
+   necessary memory, code, etc and then it needs to be wiped after the tasks
+   are completed...
+   
+   so brute force, bit flip, etc can all be distributed in ranges, and
+   it could be mixed with a variety of software so no single process fuzzes
+   the same software consistently.. this allows it to work against
+   as many programs as the injectable DLL creates information regarding
+   or allows an adware network to upload multiple versions of all apps
+   to fuzz everything users generally users
+   
+   create a server side statistics engine to keep track of all software
+   on each desktop machine and focus on the ones with higher counts
+   focus and emulate the top versions  (highest count) of each
+   application being fuzzed
+    
+
+*/
